4f2f7629975e9f2442e108ba157308a9bd444ec0
Marius GUITTON-FRANTZ <marius.guitton-frantz@etu.univ-nantes.fr>
2023-10-23 11:37:20 +0200

    add easier modularity for bash benchmark

--- becnhmark/benchmark.sh
+++ becnhmark/benchmark.sh
@@ -9,6 +9,8 @@ gsutil cp dataproc.py gs://my_own_bucket_lsdm/
 gsutil cp ./pyspark/pagerank-notype.py gs://my_own_bucket_lsdm/
 
 data="gs:///public_lddm_data/"
+my_bucket="gs://my_own_bucket_lsdm"
+project_id="true-server-401112"
 
 ## Define list of workers
 workers=(2 4 6)
@@ -17,16 +19,16 @@ workers=(2 4 6)
 for num_workers in "${workers[@]}"; do
     ##------------------ SPARK ------------------
     ## Clean out directory
-    gsutil rm -rf gs://my_own_bucket_lsdm/out/pig
-    gsutil rm -rf gs://my_own_bucket_lsdm/out/spark
+    gsutil rm -rf $my_bucket/out/pig
+    gsutil rm -rf $my_bucket/out/spark
 
     ## create the cluster
-    gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-west1 --zone europe-west1-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers $num_workers --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project true-server-401112
+    gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-west1 --zone europe-west1-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers $num_workers --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project $project_id
 
     ## run
     ## (suppose that out directory is empty !!)
     pig_start_time=$(date +%s)
-    gcloud dataproc jobs submit pig --region europe-west1 --cluster cluster-a35a -f gs://my_own_bucket_lsdm/dataproc.py
+    gcloud dataproc jobs submit pig --region europe-west1 --cluster cluster-a35a -f $my_bucket/dataproc.py
     pig_end_time=$(date +%s)
 
     ##------------------ SPARK ------------------
@@ -34,7 +36,7 @@ for num_workers in "${workers[@]}"; do
     ## run
     ## (suppose that out directory is empty !!)
     spark_start_time=$(date +%s)
-    gcloud dataproc jobs submit pyspark --region europe-west1 --cluster cluster-a35a gs://my_own_bucket_lsdm/pagerank-notype.py -- data/small_page_links.nt 3
+    gcloud dataproc jobs submit pyspark --region europe-west1 --cluster cluster-a35a $my_bucket/pagerank-notype.py -- data/small_page_links.nt 3
     spark_end_time=$(date +%s)
 
     ## delete cluster...
@@ -44,9 +46,9 @@ for num_workers in "${workers[@]}"; do
     spark_computing_time=$((spark_end_time - spark_start_time))
 
     ## access results
-    gsutil cat gs://my_own_bucket_lsdm/out/pig/pagerank_data_10/part-r-00000 >pig_results_$num_workers.txt
+    gsutil cat $my_bucket/out/pig/pagerank_data_10/part-r-00000 >pig_results_$num_workers.txt
     ## access results
-    gsutil cat gs://my_own_bucket_lsdm/out/spark/pagerank_data_10/part-r-00000 >spark_results_$num_workers.txt
+    gsutil cat $my_bucket/out/spark/pagerank_data_10/part-r-00000 >spark_results_$num_workers.txt
 
     ## write computing time to file
     echo "$num_workers workers: $pig_computing_time seconds" >>computing_times_pig.txt
