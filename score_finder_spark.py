15ab23a65d60484fc3a8223ccdd21ae1fae66c5c
Marius GUITTON-FRANTZ <marius.guitton-frantz@etu.univ-nantes.fr>
2023-10-27 11:23:36 +0200

    functionning score finder for spark results in bucket

--- score_finder_spark.py
+++ score_finder_spark.py
@@ -2,38 +2,39 @@
 
 import os
 import re
-
+from google.cloud import storage
 
 def main():
-    my_bucket = "gs://my_own_bucket_lsdm/out/spark/"
-    #foreach file in each folder in my_bucket
-    #get the best score
-    for folder in os.listdir(my_bucket):
-        print(folder)
-        for file in os.listdir(my_bucket + folder):
-            print(file)
-            if file.startswith('out'):
-                print(file)
-                file = open(my_bucket + folder + '/' + file, 'r')
-                best_score, best_url = get_best_score(file)
-                print(best_score, best_url)
-                file.close()
+    my_bucket = "my_own_bucket_lsdm"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(my_bucket)
+
+    for blob in bucket.list_blobs():
+        #find the blob that starts with out/spark/ and doesn't end with /
+        if blob.name.startswith('out/spark/') and not blob.name.endswith('/'):
+            print(blob.name)
+            best_score, best_url = get_best_score(blob)
+            print(best_score, best_url)
+
 
 def get_best_score(file):
-    best_score = 0
-    best_url = ''
-    Lines = file.readlines()
-    for line in Lines:
-        print(line.rstrip())
-
-        m = re.search(r'\((.*)\, (.*)\)', line)
-        if m:
-            score = float(m.group(2))
-            if score > best_score:
-                best_score = score
-                best_url = m.group(1)
-                
-    return best_score, best_url
+    with file.open(mode="r") as f:
+        #foreach line in file
+        # find the best score
+        # return the best score and the url
+        my_file = f.read()
+        best_score = 0
+        best_url = ''
+        Lines = my_file.splitlines()
+        for line in Lines:
+            m = re.search(r'\((.*)\, (.*)\)', line)
+            if m:
+                score = float(m.group(2))
+                if score > best_score:
+                    best_score = score
+                    best_url = m.group(1)    
+        return best_score, best_url
 
 if __name__ == "__main__":
     main()
