744fb919eb79599333a3929f46575fd9349e3704
Gabriel Pouplin <48562305+Lapin-Obez@users.noreply.github.com>
2023-10-28 21:08:34 +0200

    Update benchmark.sh

--- becnhmark/benchmark.sh
+++ becnhmark/benchmark.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
 ##------------------ SET UP ------------------
-
+data="gs://my_gabibou_bucket"
+my_bucket="gs://my_gabibou_bucket"
+project_id="projet-large-scale-data-manage"
+region=europe-west2
+zone=europe-west2-c
 ## copy pig code
-gsutil cp dataproc.py gs://my_own_bucket_lsdm/
+gsutil cp ../dataproc.py $my_bucket/
 
 ## copy spark code
-gsutil cp ./pyspark/pagerank.py gs://my_own_bucket_lsdm/
-
-data="gs://public_lddm_data"
-my_bucket="gs://my_own_bucket_lsdm"
-project_id="true-server-401112"
+gsutil cp ../pyspark/pagerank.py $my_bucket/
 
 ## Define list of workers
 workers=(2 3 4 5)
@@ -18,46 +17,46 @@ workers=(2 3 4 5)
 ## Loop over workers
 for num_workers in "${workers[@]}"; do
     ## Clean out directory
-    gsutil rm -rf $my_bucket/out/pig
-    gsutil rm -rf $my_bucket/out/spark
+    gsutil rm -rf $my_bucket/out
+    #gsutil rm -rf $my_bucket/out/spark
 
     ## create the cluster
-    gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-west1 --zone europe-west1-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers $num_workers --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project $project_id
+    gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region ${region} --zone ${zone} --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers $num_workers --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project $project_id
 
     ##------------------ PIG ------------------
 
     ## run
     ## (suppose that out directory is empty !!)
-    pig_start_time=$(date +%s%N)
-    gcloud dataproc jobs submit pig --region europe-west1 --cluster cluster-a35a -f $my_bucket/dataproc.py
-    pig_end_time=$(date +%s%N)
+    #pig_start_time=$(date +%s%N)
+    #gcloud dataproc jobs submit pig --region ${region} --cluster cluster-a35a -f $my_bucket/dataproc.py
+    #pig_end_time=$(date +%s%N)
 
     ##------------------ SPARK ------------------
 
     ## run
     ## (suppose that out directory is empty !!)
     spark_start_time=$(date +%s%N)
-    gcloud dataproc jobs submit pyspark --region europe-west1 --cluster cluster-a35a $my_bucket/pagerank.py -- $data/page_links_en.nt.bz2 3
+    gcloud dataproc jobs submit pyspark --region ${region} --cluster cluster-a35a $my_bucket/pagerank.py -- $data/page_links_en.nt.bz2 3
     spark_end_time=$(date +%s%N)
 
     ## delete cluster...
-    gcloud dataproc clusters delete cluster-a35a --region europe-west1 -Y
+    gcloud dataproc clusters delete cluster-a35a --region ${region} --quiet
 
-    python ./score_finder_spark.py >> res_spark.txt
-    python ./bestpig.py >> res_pig.txt
-    pig_computing_time=$((pig_end_time - pig_start_time))
+    python ./../score_finder_spark.py >> res_spark.txt
+    #python ./../bestpig.py >> res_pig.txt
+    #pig_computing_time=$((pig_end_time - pig_start_time))
     spark_computing_time=$((spark_end_time - spark_start_time))
 
-    pig_computing_time=$pig_computing_time/1000000
-    spark_computing_time=$spark_computing_time/1000000
+    #let "pig_computing_time=$pig_computing_time/1000000"
+    let "spark_computing_time=$spark_computing_time/1000000"
 
     ## access results
-    gsutil cat $my_bucket/out/pig/pagerank_data_10/part-r-00000 >pig_results_$num_workers.txt
+    #gsutil cat $my_bucket/out/pig/pagerank_data_10/part-r-00000 >pig_results_$num_workers.txt
     ## access results
-    gsutil cat $my_bucket/out/spark/pagerank_data_10/part-r-00000 >spark_results_$num_workers.txt
+    #gsutil cat $my_bucket/out/spark/pagerank_data_10/part-r-00000 >spark_results_$num_workers.txt
 
     ## write computing time to file
-    echo "$num_workers workers: $pig_computing_time seconds" >>computing_times_pig.txt
+    #echo "$num_workers workers: $pig_computing_time ms" >>computing_times_pig.txt
     ## write computing time to file
-    echo "$num_workers workers: $spark_computing_time seconds" >>computing_times_spark.txt
+    echo "$num_workers workers: $spark_computing_time ms" >>computing_times_spark.txt
 done
